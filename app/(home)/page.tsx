f80beda54dc4768778089514a5ceae8084d18a64
Kevin Halstead <mrmichael423@icloud.com>
2024-06-15 14:35:47 -0400

    start on main page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 import type { HeroQueryResult, SettingsQueryResult } from "@/sanity.types";
@@ -6,12 +7,54 @@ import { sanityFetch } from "@/sanity/lib/fetch";
 import { heroQuery, settingsQuery } from "@/sanity/lib/queries";
 
 export default async function Page() {
-  const [settings, heroPost] = await Promise.all([
-    sanityFetch<SettingsQueryResult>({
-      query: settingsQuery,
-    }),
-    sanityFetch<HeroQueryResult>({ query: heroQuery }),
-  ]);
+  return (
+    <div className="mainPage">
+      <section className="heroContent">
+        <h1>
+          Welcome to
+          <br /> <span>Love First Christian Center!</span>
+        </h1>
 
-  return <div className="container mx-auto px-5">HomePage</div>;
+        <div className="heroCardContainer">
+          <div className="cardContainer">
+            <Image
+              src="/in-person.webp"
+              alt="Join Love First In person"
+              height={100}
+              width={90}
+            />
+            <h2>Service Times</h2>
+            <p>Join us in person on Sundays at</p>
+            <p>7:45a | 9:45a | 11:45a</p>
+            <p>Wednesdays at 7:00p</p>
+          </div>
+          <div className="cardContainer">
+            <Image
+              src="/live2.webp"
+              alt="Watch Church Online"
+              height={50}
+              width={60}
+            />
+            <h2>Watch Online</h2>
+            <p>
+              Watch Love First online or watch any of our previous sermons now!
+            </p>
+            <Link href="/watch-online">Watch Now</Link>
+          </div>
+          <div className="cardContainer">
+            <Image
+              src="/location.webp"
+              alt="Watch Church Online"
+              height={50}
+              width={40}
+            />
+            <h2>Location</h2>
+            <p>
+              Join us in person at 12847 Balm Riverview Rd, Riverview, FL 33579.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -6,25 +6,26 @@ export default function Nav() {
   return (
     <nav className="links">
       <ul>
-        <li
-          className={`online ${pathname === "/watch-online" ? "active" : ""}`}
-        >
-          <Link href="/watch-online">Online</Link>
-        </li>
-        <li className={pathname === "/prayer" ? "active" : ""}>
-          <Link href="/prayer">Prayer</Link>
-        </li>
         <li className={pathname === "/about-love-first" ? "active" : ""}>
           <Link href="/about-love-first">About</Link>
         </li>
         <li className={pathname === "/connect" ? "active" : ""}>
           <Link href="/connect">Connect</Link>
         </li>
+        <li className={pathname === "/prayer" ? "active" : ""}>
+          <Link href="/prayer">Prayer</Link>
+        </li>
         <li className={pathname === "/give" ? "active" : ""}>
           <Link href="/give">Give</Link>
         </li>
         <li className={pathname === "/contact" ? "active" : ""}>
           <Link href="/contact">Contact</Link>
+        </li>
+
+        <li
+          className={`online ${pathname === "/watch-online" ? "active" : ""}`}
+        >
+          <Link href="/watch-online">Online</Link>
         </li>
       </ul>
     </nav>
