52a5036143f43afb7a2348820a6fe599a8b1355c
Kevin Halstead <mrmichael423@icloud.com>
2024-07-30 15:04:45 -0400

    fix console errors?

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -21,10 +21,9 @@ export default async function Page() {
   return (
     <div className="mainPage">
       <Hero serviceTime={serviceTime} />
-      <Suspense>
-        <WelcomeSection serviceTime={serviceTime} />
-        <CommunitySection />
-      </Suspense>
+
+      <WelcomeSection serviceTime={serviceTime} />
+      <CommunitySection />
       <svg
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
@@ -36,10 +35,9 @@ export default async function Page() {
           d="M0,32L48,37.3C96,43,192,53,288,90.7C384,128,480,192,576,202.7C672,213,768,171,864,165.3C960,160,1056,192,1152,218.7C1248,245,1344,267,1392,277.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
         ></path>
       </svg>
-      <Suspense>
-        <SermonSection path="home" />
-        <EventsPage />
-      </Suspense>
+
+      <SermonSection path="home" />
+      <EventsPage />
     </div>
   );
 }
