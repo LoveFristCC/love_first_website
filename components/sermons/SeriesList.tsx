d16c965b37adbcc82a197f7b3feb20fef48c92ce
Kevin Halstead <mrmichael423@icloud.com>
2024-08-03 10:44:42 -0400

    more animations

--- components/sermons/SeriesList.tsx
+++ components/sermons/SeriesList.tsx
@@ -1,14 +1,38 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useRef } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { urlForImage } from "@/sanity/lib/utils";
+import { motion, useInView } from "framer-motion";
+
+const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const items = {
+  hidden: { y: -20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+};
 
 interface SeriesListProps {
   series: any;
 }
 
 const SeriesList: React.FC<SeriesListProps> = ({ series }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   const getLastFiveItems = (array: any) => {
     if (!Array.isArray(array)) {
       throw new TypeError("The provided argument is not an array.");
@@ -22,7 +46,14 @@ const SeriesList: React.FC<SeriesListProps> = ({ series }) => {
   return (
     <div className="series-list">
       <h3>Previous Series</h3>
-      <div className="series-container">
+      <motion.div
+        className="series-container"
+        ref={ref}
+        variants={container}
+        initial="hidden"
+        animate={isInView ? "visible" : "hidden"}
+        viewport={{ amount: 0.1 }}
+      >
         {lastFiveSeries.map(
           (
             item: { seriesImage: string; route: string; title: string },
@@ -34,9 +65,11 @@ const SeriesList: React.FC<SeriesListProps> = ({ series }) => {
               .url();
 
             return (
-              <Link
+              <motion.a
                 href={`/watch-online/previous-sermons/${item.route}`}
                 key={index}
+                variants={items}
+                viewport={{ amount: 0.1 }}
               >
                 <Image
                   src={imageUrl as string}
@@ -46,11 +79,11 @@ const SeriesList: React.FC<SeriesListProps> = ({ series }) => {
                 />
 
                 <p>{item.title}</p>
-              </Link>
+              </motion.a>
             );
           }
         )}
-      </div>
+      </motion.div>
       <div className="moreSermonsCtaContainer">
         <Link className="moreSermonsCta" href="/watch-online/previous-sermons">
           View all Series
