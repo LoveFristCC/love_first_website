da998418c1375cf14bf8e3c01c1f8c56cc05cb12
Kevin Halstead <mrmichael423@icloud.com>
2024-11-19 09:01:26 -0500

    start join page

---
+++ app/(home)/join/page.tsx
@@ -0,0 +1,15 @@
+export default function Join() {
+  return (
+    <a
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        minHeight: "100vh",
+        alignItems: "center",
+      }}
+      href="https://lovefirst.churchcenter.com/people/forms/133493?open-in-church-center-modal=true"
+    >
+      Complete New Member Form
+    </a>
+  );
+}
