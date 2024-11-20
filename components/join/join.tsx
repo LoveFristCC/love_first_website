fff3fe1f369df26e684e4ffe50a102b93fdc696e
Kevin Halstead <mrmichael423@icloud.com>
2024-11-20 14:40:35 -0500

    new branch

--- components/join/join.tsx
+++ components/join/join.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 const JoinSection = () => {
+  console.log("test");
   return (
     <section className="joinSection">
       <div className="joinContentContainer">
