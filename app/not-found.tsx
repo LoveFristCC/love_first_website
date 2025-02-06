23a87ff6217d3c56980820b2534e529455fc025c
Kevin Halstead <mrmichael423@icloud.com>
2025-02-06 08:35:49 -0500

    update to redirect

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,19 +1,5 @@
-import { redirect } from "@/sanity/lib/queries";
-import { sanityFetch } from "@/sanity/lib/fetch";
 import Redirect from "@/components/RedirectComponent";
 
 export default async function NotFound() {
-  const [redirectLinks] = await Promise.all([
-    sanityFetch<any>({
-      query: redirect,
-      // params: {},
-      // perspective: "published",
-      // stega: false,
-      // timeout: 0,
-      // cdn: false,
-    }),
-  ]);
-  console.log("🚀 ~ redirectLinks:", redirectLinks);
-
-  return <Redirect redirectLinks={redirectLinks} />;
+  return <Redirect />;
 }
--- components/RedirectComponent.tsx
+++ components/RedirectComponent.tsx
@@ -1,26 +1,41 @@
 "use client";
 import { useEffect } from "react";
 import { usePathname, useRouter } from "next/navigation";
+import { client } from "@/sanity/lib/client";
+import { redirect } from "@/sanity/lib/queries";
 
-export default function Redirect({ redirectLinks }: { redirectLinks: any }) {
-  console.log("🚀 ~ redirectLinks:", redirectLinks);
+export default function Redirect() {
   const pathname = usePathname();
   const router = useRouter();
 
   useEffect(() => {
-    const matchedRedirectLink = redirectLinks.find(
-      (el: any) => el.title === pathname || ""
-    );
-    console.log("🚀 ~ matchedRedirectLink:", matchedRedirectLink);
+    (async () => {
+      const data = await client.fetch(
+        redirect,
+        {},
+        {
+          perspective: "published",
+          // The `published` perspective is available on the API CDN
+          useCdn: false,
+          // When using the `published` perspective we use time-based revalidation to match the time-to-live on Sanity's API CDN (60 seconds)
+          next: { revalidate: 0 },
+        }
+      );
+      console.log("🚀 ~ data:", data);
+      const matchedRedirectLink = data.find(
+        (el: any) => el.title === pathname || ""
+      );
+      console.log("🚀 ~ matchedRedirectLink:", matchedRedirectLink);
 
-    if (matchedRedirectLink) {
-      router.push(matchedRedirectLink.redirectUrl);
-    } else {
-      router.push("/");
-    }
+      if (matchedRedirectLink) {
+        router.push(matchedRedirectLink.redirectUrl);
+      } else {
+        router.push("/");
+      }
+    })();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname]);
+  }, []);
 
   return <div className="redirectContainer"></div>;
 }
